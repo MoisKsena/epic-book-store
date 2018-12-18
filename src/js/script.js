8ccbe332653ea125fc3b40b74b25e7c5c86c42a6
Kseniia Moiseeva <mailtoksena@gmail.com>
2018-12-18 21:35:06 +0300

    script

--- src/js/script.js
+++ src/js/script.js
@@ -5,47 +5,37 @@ import pageManager from './modules/pageManager.js';
 import requestManager from './modules/requestManager.js';
 import catalogManager from './modules/catalogManager.js';
 
+function bookPage(response){
+  const request = JSON.parse(response);
 
+  const wrap = document.querySelector(bookCardTemplate.wrap);
+
+  if (wrap.children){
+    wrap.innerHTML = '';
+  }
+
+  if (document.querySelector(bookCardTemplate.wrap)) {
+    console.log(request.items);
+    addPage(request.items, bookCardTemplate);
+  }
+}
 
 tabManager.addTabClickListener(function(event) {
       event.preventDefault();
       const url = pageManager.prepareUrl(event.target.dataset.type);
-      requestManager.send(url, function(response) {
-      const request = JSON.parse(response);
-
-      const wrap = document.querySelector(bookCardTemplate.wrap);
-
-      if (wrap.children){
-        wrap.innerHTML = '';
-      }
-
-      if (document.querySelector(bookCardTemplate.wrap)) {
-        console.log(request.items);
-        addPage(request.items, bookCardTemplate);
-      }
-    },
-    function(xhr){
-      console.log(`wait dawnload: ${xhr.readyState}`);
-    });
+      requestManager.send(url,
+        bookPage,
+        function(xhr){
+          console.log(`wait download: ${xhr.readyState}`);
+        });
   });
 
 
-requestManager.send(pageManager.prepareUrl(), function(response) {
-    const request = JSON.parse(response);
-    const wrap = document.querySelector(bookCardTemplate.wrap);
-
-    if (wrap.children){
-      wrap.innerHTML = '';
-    }
-
-    if (document.querySelector(bookCardTemplate.wrap)) {
-      console.log(request.items);
-      addPage(request.items, bookCardTemplate);
-    }
-  },
+requestManager.send(pageManager.prepareUrl(),
+  bookPage,
 
   function(xhr){
-    console.log(`wait dawnload: ${xhr.readyState}`);
+    console.log(`wait download: ${xhr.readyState}`);
   }
 );
 
@@ -53,18 +43,9 @@ var pi = document.querySelector('.page__inner');
 pi.innerHTML = pageManager.renderPager(17,25);
 
 
-/*catalogManager.addTabClickListener(function(event){
-    event.preventDefault();
-
-    const url = pageManager.prepareUrl(event.target.dataset.type);
-
-    requestManager.send(url);
-
-  });
-*/
 /*swiper*/
 
-/*  var mySwiper = new Swiper ('.slider', {
+  /*var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
     direction: 'vertical',
     loop: true,*/
@@ -75,14 +56,14 @@ pi.innerHTML = pageManager.renderPager(17,25);
     },*/
 
     // Navigation arrows
-/*    navigation: {
+    /*navigation: {
       nextEl: '.slider__button-next',
       prevEl: '.slider__button-prev',
     },*/
-
     // And if we need scrollbar
    /* scrollbar: {
       el: '.swiper-scrollbar',
     },*/
-/*  })*/
+
+/*  });*/
 
